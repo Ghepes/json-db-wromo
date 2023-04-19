6f74d65d17d376f8da70d7ade01613ea04494de4
Ersin84 <info.wromo@gmail.com>
2023-04-19 08:22:05 +0200

    add

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const data = {
     products: [],
   };
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100; i++) {
     data.products.push({
       id: i,
       title: `products${i}`,
